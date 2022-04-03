c895b2570d83814b9be500cf4e257ab2b6e6d4dd
VIJAYAVARSHINI <101562511+VIJAYAVARSHINI@users.noreply.github.com>
2022-04-03 15:19:24 +0530

    Update day.h

--- 3_Implementation/inc/day.h
+++ 3_Implementation/inc/day.h
@@ -12,15 +12,31 @@
 #ifndef _DAY_H_
 #define _DAY_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 int validateDate(int d, int m, int y);
+
+/**
+ * @brief Validating the provided date
+ * @param[in] operand1 Date - DD format
+ * @param[in] operand2 Month - MM format
+ * @param[in] operand3 Year - YYYY format
+ * @return Result 0 or 1
+ * 
+ */
+
 
 int wd(int year, int month, int day);
 
 /**
- * @brief Validating the day of the week
- * @d  date[in] Date format
- * @m Month[in] Month in the required mm format
- * @y Year[in] Year in the yyyy format
+ * @brief Calculating the day of the week
+ * @param[in] operand1 Date - DD format
+ * @param[in] operand2 Month - MM format
+ * @param[in] operand3 Year - YYYY format
+ * @return Result [0 to 6]
  * 
  */
 
